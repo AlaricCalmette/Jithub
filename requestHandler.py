cbdf24a1c67296a881d04fce9edc40f3cafa27a9
Alaric Calmette <alaric.calmette@gmail.com>
2018-10-24 19:47:42 +0200

    chore(request handler): refacto and error handling

--- requestHandler.py
+++ requestHandler.py
@@ -2,6 +2,7 @@ from http.server import BaseHTTPRequestHandler
 import json
 import re
 from jira import JIRA
+import traceback
 import pdb
 
 with open('config.json') as config:
@@ -21,54 +22,48 @@ oauth_dict = {
 }
 jira = JIRA(config_dict['jira']['instance_address'], oauth=oauth_dict)
 
-def handle_labeled(body):
-  print('label %s added to pr %s' %(body['label']['name'], body['pull_request']['title']))
+def handle_labeled_unlabeled(body, action):
+  added_or_removed = 'added' if action == 'labeled' else 'removed'
+  print('label %s %s to pr %s' %(
+  body['label']['name'], added_or_removed, body['pull_request']['title']))
   test = project_regex.search(body['pull_request']['title'])
-  if test:
-    ticket = test.group(0)
-    print('Should act on jira ticket %s' %(ticket))
-    jira_ticket = jira.issue(ticket)
-    transition = config_dict['action_mapping']['labeled'][body['label']['name']]
-    transitionName = transition['name']
-    transitionId = jira.find_transitionid_by_name(jira_ticket.id, transitionName)
-    try:
-      jira_ticket.update(transition['fields'])
-    except KeyError:
-      print('No fields provided for %s' %(transitionName))
-    except:
-      print('An unknown error occured while trying to update fields of %s' %(ticket))
-    jira.transition_issue(jira_ticket, transitionId, fields=None, comment='jithub')
-    print('%s has been applied on %s' %(transitionName, jira_ticket.key))
 
-def handle_unlabeled(body):
-  print('label %s removed from %s' %(body['label']['name'], body['pull_request']['title']))
-  test = project_regex.search(body['pull_request']['title'])
   if test:
     ticket = test.group(0)
     print('Should act on jira ticket %s' %(ticket))
     jira_ticket = jira.issue(ticket)
-    transition = config_dict['action_mapping']['unlabeled'][body['label']['name']]
-    transitionName = transition['name']
-    transitionId = jira.find_transitionid_by_name(jira_ticket.id, transitionName)
+
     try:
-      jira_ticket.update(transition['fields'])
+      transition = config_dict['action_mapping'][action][body['label']['name']]
+      transitionName = transition['name']
     except KeyError:
-      print('No fields provided for %s' %(transitionName))
-    except:
-      print('An unknown error occured while trying to update fields of %s' %(ticket))
-    jira.transition_issue(jira_ticket, transitionId, fields=None, comment='jithub')
+      raise Exception('No config matching label %s was found' %(body['label']['name']))
+
+    transitionId = jira.find_transitionid_by_name(jira_ticket.id, transitionName)
+    if transitionId is not None:
+      try:
+        jira_ticket.update(transition['fields'])
+      except KeyError:
+        print('No fields provided for %s' %(transitionName))
+      except Exception:
+        print('An unknown error occured while trying to update fields of %s' %(ticket))
+    else:
+      raise Exception('Transition %s does not exist or is not available for %s' %(transitionName, jira_ticket.key))
+
+    jira.transition_issue(jira_ticket, transitionId, comment='jithub')
     print('%s has been applied on %s' %(transitionName, jira_ticket.key))
 
 def handle_action(body):
   actions = {
-    'labeled': handle_labeled,
-    'unlabeled': handle_unlabeled
+    'labeled': handle_labeled_unlabeled,
+    'unlabeled': handle_labeled_unlabeled
   }
   try:
-    pdb.set_trace()
-    actions[body['action']](body)
+    actions[body['action']](body, body['action'])
   except KeyError:
     raise NotImplementedError('Action %s not yet implemented' %(body['action']))
+  except Exception as e:
+    raise NotImplementedError(e)
 
 class GithubRequestHandler(BaseHTTPRequestHandler):
 
@@ -82,6 +77,7 @@ class GithubRequestHandler(BaseHTTPRequestHandler):
       self.send_header('Content-type', 'text/html')
       self.end_headers()
     except NotImplementedError as error:
+      print(traceback.format_exc())
       self.send_error(501, str(error))
       self.send_header('Content-type', 'text/html')
       self.end_headers()

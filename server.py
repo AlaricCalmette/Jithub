c732055658be390f75ba85aae1ad76cc27d33eb5
Alaric Calmette <alaric.calmette@gmail.com>
2018-11-30 10:56:39 +0100

    fix(heroku): use env variable for port

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import json
+import os
 from http.server import HTTPServer
 from requestHandler import GithubRequestHandler
 
 with open('serverConfig.json') as serverConfig:
   serverConfig_dict = json.load(serverConfig)
-  SERVER_ADDRESS = (serverConfig_dict['address'], serverConfig_dict['port'])
+  SERVER_ADDRESS = (serverConfig_dict['address'], int(os.environ['PORT']))
 
 httpd = HTTPServer(SERVER_ADDRESS, GithubRequestHandler)
 httpd.serve_forever()
